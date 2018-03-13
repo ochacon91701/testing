acbba2a11fd18c7248f61796abb1101997bda02c
ochacon91701 <37346843+ochacon91701@users.noreply.github.com>
2018-03-13 12:54:53 -0700

    array.ps1
    v 1.0

---
+++ array.ps1
@@ -0,0 +1,13 @@
+$a = "cat", "dog", "bull"
+$a.count
+$a.GetType()
+clear
+echo "==============="
+echo "Before"
+echo "==============="
+$a
+echo "==============="
+echo "After"
+echo "==============="
+$a += "bird"
+$a
